f468369e5a1fb413b0b1339b8f31994bd15f8a61
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2022-05-20 18:52:33 -0400

    Upload Image with Cloudinary

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cca1833f2b826f51"; }
+/******/ 		__webpack_require__.h = function() { return "4d9fbb881900bb80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,9 +2,9 @@ import dbConnect from "../../../util/mongo";
 import Product from "../../../models/Product";
 
 export default async function handler(req, res) {
-  const { method } = req;
+  const { method, cookies } = req;
 
-//   const token = cookies.token
+  const token = cookies.token
 
   dbConnect();
 
@@ -18,10 +18,10 @@ export default async function handler(req, res) {
   }
 
   if (method === "POST") 
-//   {
-//     // if(!token || token !== process.env.token){
-//     //   return res.status(401).json("Not authenticated!")
-//     }
+  {
+    if(!token || token !== process.env.token){
+      return res.status(401).json("Not authenticated!")
+    }
     try {
       const product = await Product.create(req.body);
       res.status(201).json(product);
@@ -29,3 +29,4 @@ export default async function handler(req, res) {
       res.status(500).json(err);
     }
   }
+}
